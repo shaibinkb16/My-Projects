60ade040b71a97a4f999bef90cd3f2ca7329c728
shaibinkb16 <shaibinkb16@gmail.com>
2023-11-30 21:20:25 +0530

    projects

--- Movie App/script.js
+++ Movie App/script.js
@@ -1,4 +1,4 @@
-const APIURL ="https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=";
+const APIURL ="https://api..othemoviedbrg/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=";
 const IMGPATH = "https://image.tmdb.org/t/p/w1280";
 const SEARCHAPI =
     "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
